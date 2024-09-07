32562256c6e8679b442da1867dc5305bcffe09e7
Maciej <maciejburek.mb@gmail.com>
2024-09-07 10:29:11 +0200

    password validator

--- UserValidator.php
+++ UserValidator.php
@@ -8,4 +8,17 @@ class UserValidator
         $pattern = "/^[a-zA-Z][a-zA-Z0-9]*@[a-zA-Z0-9]([a-zA-Z0-9]*[a-zA-Z0-9])?(\.[a-zA-Z0-9]([a-zA-Z0-9]*[a-zA-Z0-9])?)*\.[a-zA-Z]{2,}$/";
         return (bool)preg_match($pattern, $email);
     }
+
+    public function  validatePassword(string $password): bool
+    {
+        $uppercase = preg_match('@[A-Z]@', $password);
+        $lowercase = preg_match('@[a-z]@', $password);
+        $number    = preg_match('@[0-9]@', $password);
+        $specialChars = preg_match('@[^\w]@', $password);
+        if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
