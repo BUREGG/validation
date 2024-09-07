9344876caee09df73cf45c6ced170d9f3425dd55
Maciej <maciejburek.mb@gmail.com>
2024-09-07 14:46:44 +0200

    minLenghtPassword method change

--- UserValidator.php
+++ UserValidator.php
@@ -8,15 +8,15 @@ class UserValidator
         $pattern = "/^[a-zA-Z][a-zA-Z0-9]*@[a-zA-Z0-9]([a-zA-Z0-9]*[a-zA-Z0-9])?(\.[a-zA-Z0-9]([a-zA-Z0-9]*[a-zA-Z0-9])?)*\.[a-zA-Z]{2,}$/";
         return (bool)preg_match($pattern, $email);
     }
-
-    public function  validatePassword(string $password): bool
+    public function __construct(private readonly int $minPasswordLength = 8) {}
+    public function validatePassword(string $password): bool
     {
         $uppercase = preg_match('@[A-Z]@', $password);
         $lowercase = preg_match('@[a-z]@', $password);
         $number    = preg_match('@[0-9]@', $password);
         $specialChars = preg_match('@[^\w]@', $password);
         return match (true) {
-            !$uppercase, !$lowercase, !$number, !$specialChars, strlen($password) < 8 => false,
+            !$uppercase, !$lowercase, !$number, !$specialChars, strlen($password) < $this->minPasswordLength => false,
             default => true,
         };
     }
